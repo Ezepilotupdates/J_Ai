786b7251edb5150edf097556ae0f503fb13aea9b
Tamunobarafiri Ezekiel <tamunobarafiriprecious@gmail.com>
2025-11-16 05:57:09 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,13 @@
-# content from utils.py
+# app/utils.py
+from PyPDF2 import PdfReader
+import io
+
+def extract_text_from_pdf_bytes(b: bytes) -> str:
+    try:
+        reader = PdfReader(io.BytesIO(b))
+        return "\n\n".join([p.extract_text() or "" for p in reader.pages])
+    except:
+        return ""
+
+def send_health_check():
+    return {"status": "ok"}
